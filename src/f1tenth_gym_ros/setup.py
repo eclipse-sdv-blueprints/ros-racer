bc68240fe94ceb720cdac42c393e7da77e850ecf
Ibrahim Sel <ibrahim.sel@eteration.com>
2025-05-05 17:21:34 +0300

    initial commit

---
+++ src/f1tenth_gym_ros/setup.py
@@ -0,0 +1,49 @@
+#
+# Copyright (c) 2025 Composiv.ai
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Composiv.ai - initial API and implementation
+#
+
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'f1tenth_gym_ros'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.xacro')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.rviz')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*.yaml')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*.png')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*.pgm')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='composiv',
+    maintainer_email='info@composiv.ai',
+    description='Bridge with multiagent support for using f1tenth_gym in ROS2',
+    license='EPL-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'gym_bridge = f1tenth_gym_ros.gym_bridge:main',
+            'gap_follower = f1tenth_gym_ros.gap_follower:main',
+        ],
+    },
+)
